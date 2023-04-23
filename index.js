645d6101d824b36c815af444fa333676024dbb22
mathurharshgeek <harshonlaptop@gmail.com>
2023-04-23 16:21:47 +0530

    uploaded code

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require('express')
+const path=require('path');
+const ejs= require('ejs');
+const multer=require('multer');
+const app = express();
+const publicDir = path.join(__dirname, "/public");
+app.use(express.static(publicDir));
+app.use(express.urlencoded({ extended: true })); // So we can access the form input values
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set('views',path.join(__dirname,'/views'));
+app.get('/',(req,res)=>res.render('home'));
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    return cb (null, "./public/images");
+    },
+    filename: function (req, file, cb) {
+    return cb (null, `${Date.now()}-${file.originalname}`);
+    },
+});
+const upload = multer({ storage });
+app.post("/", upload.single("image"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    return res.render('show',{data:req.file.filename});
+});
+
+app.listen(80,()=>{
+    console.log("server has started");
+})
